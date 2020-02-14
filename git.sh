9ca624613bfdda4abd816912f7132084cd34e1a4
synod2 <synod2@naver.com>
2020-02-14 08:53:22 +0000

    desktop

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-msg="$(date '+%m')$(date '+%d')_notebook"
+msg="$(date '+%m')$(date '+%d')_desktop"
 
 git add .
 git commit -m $msg 
-git push -u nulcon2020 notebook
+git push -u nulcon2020 desktop
